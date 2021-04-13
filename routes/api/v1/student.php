5c4a004a05fe4b5b46ee46f82ed227b0d2553a63
Indra <indrawahyudi2710@gmail.com>
2021-04-13 16:49:39 +0800

    update student

--- routes/api/v1/student.php
+++ routes/api/v1/student.php
@@ -7,5 +7,9 @@ $api->version('v1', [
     'middleware' => ['auth:api', 'role:student|admin'],
     'prefix' => 'v1/student'
 ], function($api) {
-    
+    $api->get('/', ['as' => 'student.index', 'uses' => 'StudentController@index']);
+    $api->get('/{id}', ['as' => 'student.show', 'uses' => 'StudentController@show']);
+    $api->put('/{id}', ['as' => 'student.update', 'uses' => 'StudentController@update']);
+    $api->post('/', ['as' => 'student.create', 'uses' => 'StudentController@create']);
+    $api->delete('/{id}', ['as' => 'student.delete', 'uses' => 'StudentController@delete']);
 });
