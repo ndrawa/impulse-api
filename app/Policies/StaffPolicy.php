fef886e28f9759edbf3edd5bf20e4f9d3a2f35c9
Fariz <frzrzky@gmail.com>
2021-04-14 22:42:48 +0700

    Add few fix

--- app/Policies/StaffPolicy.php
+++ app/Policies/StaffPolicy.php
@@ -11,7 +11,7 @@ class StaffPolicy extends BasePolicy
     public function update(User $user, Staff $staff)
     {
         // admin can update all staff
-        if($user->isAdmin()) {
+        if($user->isLaboran()) {
             return $this->allow();
         } else if($user->id == $staff->user->id) {
             // only the logged user can update himself
@@ -24,13 +24,13 @@ class StaffPolicy extends BasePolicy
     public function create(User $user)
     {
         // only admin can create staff
-        return $this->authorize($user->isAdmin());
+        return $this->authorize($user->isLaboran());
     }
 
     public function delete(User $user, Staff $staff)
     {
         // only admin can delete staff & admin can't delete himself
-        if($user->isAdmin() && $user->id != $staff->user->id) {
+        if($user->isLaboran() && $user->id != $staff->user->id) {
             return $this->allow();
         } else {
             return $this->deny();
--- app/Policies/StudentPolicy.php
+++ app/Policies/StudentPolicy.php
@@ -10,7 +10,7 @@ class StudentPolicy extends BasePolicy
     public function update(User $user, Student $student)
     {
         // admin can update all student
-        if($user->isAdmin()) {
+        if($user->isLaboran()) {
             return $this->allow();
         } else if($user->id == $student->user->id) {
             // only the logged user can update himself
@@ -23,13 +23,13 @@ class StudentPolicy extends BasePolicy
     public function create(User $user)
     {
         // only admin can create student
-        return $this->authorize($user->isAdmin());
+        return $this->authorize($user->isLaboran());
     }
 
     public function delete(User $user)
     {
         // only admin can delete student
-        if($user->isAdmin()) {
+        if($user->isLaboran()) {
             return $this->allow();
         } else {
             return $this->deny();
