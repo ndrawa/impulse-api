b2949e14077b4710b8eeb634ec6db73a8b1a5135
Fariz <frzrzky@gmail.com>
2021-04-20 15:39:59 +0700

    model update

---
+++ app/Models/Schedule.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends BaseModel
+{
+    protected $table = "schedules";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'day',
+        'time_start',
+        'time_end',
+        'periode_start',
+        'periode_end',
+    ];
+    // module_id belum dimasukkan, menunggu instruksi lebih lanjut karena
+    // ada relasi ke tabel yg belum dibuat
+
+    public function rooms()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function classes()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+}
