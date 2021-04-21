cf23e87db07bf221e9d3e3a99d51642e84c3181e
Fariz <frzrzky@gmail.com>
2021-04-21 22:04:19 +0700

    class,room,schedule policy

---
+++ app/Policies/ClassroomPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ClassroomPolicy extends BasePolicy
+{
+    public function update(User $user)
+    {
+        // Laboran can update all classes
+        if($user->isLaboran()) {
+            return $this->allow();
+        } else {
+            return $this->deny();
+        }
+    }
+
+    public function create(User $user)
+    {
+        // only laboran can create classes
+        return $this->authorize($user->isLaboran());
+    }
+
+    public function delete(User $user)
+    {
+        // only labran can delete class
+        if($user->isLaboran()) {
+            return $this->allow();
+        } else {
+            return $this->deny();
+        }
+    }
+}
---
+++ app/Policies/RoomPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RoomPolicy extends BasePolicy
+{
+    public function update(User $user)
+    {
+        // Laboran can update all rooms
+        if($user->isLaboran()) {
+            return $this->allow();
+        } else {
+            return $this->deny();
+        }
+    }
+
+    public function create(User $user)
+    {
+        // only laboran can create rooms
+        return $this->authorize($user->isLaboran());
+    }
+
+    public function delete(User $user)
+    {
+        // only labran can delete room
+        if($user->isLaboran()) {
+            return $this->allow();
+        } else {
+            return $this->deny();
+        }
+    }
+}
