7a2cd67dd65541c14613bbcf45db01f2771db32d
Indra <indrawahyudi2710@gmail.com>
2021-05-02 18:04:29 +0800

    update classes

--- app/Http/Controllers/Api/V1/Classroom/ClassroomController.php
+++ app/Http/Controllers/Api/V1/Classroom/ClassroomController.php
@@ -12,21 +12,67 @@ class ClassroomController extends BaseController
 {
     public function index(Request $request)
     {
-        $classrooms = Classroom::query();
-        $per_page = env('PAGINATION_SIZE', 15);
-        $request->whenHas('per_page', function($size) use (&$per_page) {
-            $per_page = $size;
-        });
+        $classrooms = Classroom::query()->get();
+        // $per_page = env('PAGINATION_SIZE', 15);
+        // $request->whenHas('per_page', function($size) use (&$per_page) {
+        //     $per_page = $size;
+        // });
 
-        $request->whenHas('search', function($search) use (&$staffs) {
-            $classrooms = $classrooms->where('name', 'LIKE', '%'.$search.'%');
-        });
+        // $request->whenHas('search', function($search) use (&$staffs) {
+        //     $classrooms = $classrooms->where('name', 'LIKE', '%'.$search.'%');
+        // });
 
-        $classrooms = $classrooms->paginate($per_page);
+        // $classrooms = $classrooms->paginate($per_page);
 
-        return $this->response->paginator($classrooms, new ClassroomTransformer);
+        return $this->response->item($classrooms, new ClassroomTransformer);
     }
 
+    public function show(Request $request, $id)
+    {
+        $classroom = Classroom::findOrFail($id);
+        return $this->response->item($classroom, new ClassroomTransformer);
+    }
+
+    public function create(Request $request)
+    {
+        // $this->authorize('create', Classroom::class);
+        $this->validate($request, [
+            'staff_id' => 'required',
+            'name' => 'required',
+            'course_id' => 'required',
+            'academic_year' => 'required',
+            'semester' => 'required'
+        ]);
+        $classroom = Classroom::create($request->all());
+
+        return $this->response->item($classroom, new ClassroomTransformer);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $classroom = Classroom::findOrFail($id);
+        // $this->authorize('update', $classroom);
+        $this->validate($request, [
+            'staff_id' => 'required',
+            'name' => 'required',
+            'course_id' => 'required',
+            'academic_year' => 'required',
+            'semester' => 'required'
+        ]);
+        $classroom->fill($request->all());
+        $classroom->save();
+
+        return $this->response->item($classroom, new ClassroomTransformer);
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $classroom = Classroom::findOrFail($id);
+        // $this->authorize('delete', $classroom);
+        $classroom->delete();
+
+        return $this->response->noContent();
+    }
     // without paginator
     // public function rooms(Request $request)
     // {
--- routes/api/v1/classroom.php
+++ routes/api/v1/classroom.php
@@ -8,4 +8,8 @@ $api->version('v1', [
     'prefix' => 'v1/classroom'
 ], function($api) {
     $api->get('/', ['as' => 'classroom.index', 'uses' => 'ClassroomController@index']);
+    $api->get('/{id}', ['as' => 'classroom.show', 'uses' => 'ClassroomController@show']);
+    $api->put('/{id}', ['as' => 'classroom.update', 'uses' => 'ClassroomController@update']);
+    $api->post('/', ['as' => 'classroom.create', 'uses' => 'ClassroomController@create']);
+    $api->delete('/{id}', ['as' => 'classroom.delete', 'uses' => 'ClassroomController@delete']);
 });
