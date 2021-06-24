66a682b6adc3fcf79e2634bb99ea2af0b25e30bf
Fariz <frzrzky@gmail.com>
2021-06-24 20:22:38 +0700

    tables for journal/test/answer

---
+++ database/migrations/2021_06_24_124700_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->char('id', 26)->primary();
+            $table->timestamp('time_start');
+            $table->timestamp('time_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
