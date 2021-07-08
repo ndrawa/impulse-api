a15e11cbaa85b0c00672603671f134ba61fee0be
Fariz <frzrzky@gmail.com>
2021-07-08 15:11:50 +0700

    models (grade & scheduletest)

---
+++ app/Models/ScheduleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ScheduleTest extends BaseModel
+{
+    protected $fillable = [
+        'schedule_id',
+        'test_id',
+        'time_start',
+        'time_end',
+        'is_active',
+        'auth',
+    ];
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
+
+    public function test()
+    {
+        return $this->belongsTo(Test::class);
+    }
+}
