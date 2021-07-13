ae73a326332f56e25b7b9eb442b3885bf1afaebd
Fariz <frzrzky@gmail.com>
2021-07-13 15:21:31 +0700

    the seeds

---
+++ database/seeders/ScheduleTestTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ScheduleTest;
+
+class ScheduleTestTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Usage: to create schedule test and test ()
+    }
+}
