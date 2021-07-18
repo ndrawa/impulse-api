5ed794cb4f3c8d1f7957bc3a75adbc45c1f1af5f
Indra <indrawahyudi2710@gmail.com>
2021-07-18 18:26:28 +0800

    Generate Schedules and Modules

--- app/Http/Controllers/Api/V1/Student/LaboranController.php
+++ app/Http/Controllers/Api/V1/Student/LaboranController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Api\V1\Student;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\V1\BaseController;
-use App\Models\Student;
-use App\Models\StudentClassCourse;
-use App\Models\Staff;
+use App\Models\AcademicYear;
 use App\Models\Classroom;
 use App\Models\ClassCourse;
 use App\Models\Course;
+use App\Models\Module;
+use App\Models\Room;
+use App\Models\Schedule;
+use App\Models\Student;
+use App\Models\StudentClassCourse;
+use App\Models\Staff;
 use App\Models\User;
-use App\Models\AcademicYear;
 use App\Transformers\LaboranTransformer;
 use App\Transformers\StudentTransformer;
 use App\Transformers\StudentClassCourseTransformer;
@@ -229,6 +232,38 @@ class LaboranController extends BaseController
                 'staff_id' => $fstaff_id,
                 'academic_year_id' => $facademic_year_id
             ]);
+
+            //Generate Module 1-14
+            for ($i = 1; $i < 15; $i++) {
+                $module = '';
+                if (Module::where('course_id', $fcourse_id)->where('index', $i)->first() == null) {
+                    $module = Module::create([
+                        'course_id' => $fcourse_id,
+                        'index' => $i,
+                        'academic_year_id' => $facademic_year_id,
+                    ]);
+                    $module->save();
+                } else {
+                    $module = Module::where('course_id', $fcourse_id)->where('index', $i)->first();
+                }
+
+                //Generate Schedule 1-14
+                $schedule = Schedule::create([
+                    'name' => 'Module '.$i,
+                    'time_start' => '2021-01-12 07:30:00',
+                    'time_end' => '2021-01-12 10:30:00',
+                    'room_id' => Room::first()->id,
+                    'class_course_id' => ClassCourse::where('class_id', $fclass_id)
+                                        ->where('course_id', $fcourse_id)
+                                        ->where('staff_id', $fstaff_id)
+                                        ->where('academic_year_id', $facademic_year_id)
+                                        ->first()->id,
+                    'module_id' => $module->id,
+                    'academic_year_id' => $facademic_year_id,
+                    'date' => \Carbon\Carbon::now()->toDateTimeString(),
+                ]);
+                $schedule->save();
+            }
         }
 
         // create Student classes
--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -2,14 +2,17 @@
 
 namespace App\Imports;
 
-use App\Models\Staff;
-use App\Models\Course;
-use App\Models\Student;
-use App\Models\StudentClass;
+use App\Models\AcademicYear;
 use App\Models\Classroom;
 use App\Models\ClassCourse;
+use App\Models\Course;
+use App\Models\Module;
+use App\Models\Room;
+use App\Models\Schedule;
+use App\Models\Staff;
+use App\Models\Student;
+use App\Models\StudentClass;
 use App\Models\StudentClassCourse;
-use App\Models\AcademicYear;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
@@ -121,6 +124,38 @@ class StudentImport implements ToCollection
                             'academic_year_id' => $facademic_year_id
                         ]);
                         $classcourse->save();
+            
+                        //Generate Module 1-14
+                        for ($i = 1; $i < 15; $i++) {
+                            $module = '';
+                            if (Module::where('course_id', $fcourse_id)->where('index', $i)->first() == null) {
+                                $module = Module::create([
+                                    'course_id' => $fcourse_id,
+                                    'index' => $i,
+                                    'academic_year_id' => $facademic_year_id,
+                                ]);
+                                $module->save();
+                            } else {
+                                $module = Module::where('course_id', $fcourse_id)->where('index', $i)->first();
+                            }
+
+                            //Generate Schedule 1-14
+                            $schedule = Schedule::create([
+                                'name' => 'Module '.$i,
+                                'time_start' => '2021-01-12 07:30:00',
+                                'time_end' => '2021-01-12 10:30:00',
+                                'room_id' => Room::first()->id,
+                                'class_course_id' => ClassCourse::where('class_id', $fclass_id)
+                                                    ->where('course_id', $fcourse_id)
+                                                    ->where('staff_id', $fstaff_id)
+                                                    ->where('academic_year_id', $facademic_year_id)
+                                                    ->first()->id,
+                                'module_id' => $module->id,
+                                'academic_year_id' => $facademic_year_id,
+                                'date' => \Carbon\Carbon::now()->toDateTimeString(),
+                            ]);
+                            $schedule->save();
+                        }
                     }
 
                     // create Student classes
