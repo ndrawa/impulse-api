93e38fcef657acf8130936e97c1f38c62e6b40de
Fariz <frzrzky@gmail.com>
2021-08-12 19:46:57 +0700

    rewrite scheduletransformer

--- app/Transformers/ScheduleTransformer.php
+++ app/Transformers/ScheduleTransformer.php
@@ -16,12 +16,10 @@ class ScheduleTransformer extends TransformerAbstract
             'time_start' => $schedule->time_start,
             'time_end' => $schedule->time_end,
             'room_id' => $schedule->room_id,
-            'periode_start' => $schedule->periode_start,
-            'periode_end' => $schedule->periode_end,
             'class_course_id' => $schedule->class_course_id,
-            'class_id' => $schedule->class_id,
             'module_id' => $schedule->module_id,
-            'type' => $schedule->type
+            'academic_year_id' => $schedule->academic_year_id,
+            'date' => $schedule->date
         ];
     }
 }
