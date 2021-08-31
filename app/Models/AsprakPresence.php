33f31e640ddad7a532ec4ddf1c2c294c5c022e70
Indra <indrawahyudi2710@gmail.com>
2021-08-31 22:58:15 +0800

    update model AsprakPresence

--- app/Models/AsprakPresence.php
+++ app/Models/AsprakPresence.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AsprakPresence extends Model
+class AsprakPresence extends BaseModel
 {
     protected $table = "asprak_presence";
 
