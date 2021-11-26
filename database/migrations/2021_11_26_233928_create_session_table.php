4c266a07cc3cab7e0109e34a90c1978024dd97a3
Indra <indrawahyudi2710@gmail.com>
2021-11-26 23:52:16 +0700

    update table session

---
+++ database/migrations/2021_11_26_233928_create_session_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->char('id', 26)->primary();
+            $table->char('user_id', 26);
+            $table->string('token');
+            $table->string('user_agent');
+            $table->dateTime('login_at');
+            $table->timestamps();
+            
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
